2dd704c40845e1707390d4150ca0d1fa9f545df9
ryanojh <s10223647@connect.np.edu.sg>
2022-05-09 20:15:46 +0800

    Made changes to fragment

--- app/src/main/java/sg/edu/np/mad/practical2/Fragment1.java
+++ app/src/main/java/sg/edu/np/mad/practical2/Fragment1.java
@@ -10,10 +10,13 @@ import android.view.ViewGroup;
 
 public class Fragment1 extends Fragment {
 
+    View view;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false);
+        view =  inflater.inflate(R.layout.fragment_1, container, false);
+        return view;
     }
 }
--- app/src/main/java/sg/edu/np/mad/practical2/MessageGroup.java
+++ app/src/main/java/sg/edu/np/mad/practical2/MessageGroup.java
@@ -11,32 +11,27 @@ import android.widget.Button;
 
 public class MessageGroup extends AppCompatActivity {
 
+
     Button firstButton, secondButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_group);
-        firstButton = findViewById(R.id.group1);
+        firstButton= findViewById(R.id.group1);
         secondButton = findViewById(R.id.group2);
 
         firstButton.setOnClickListener(new View.OnClickListener() {
-            class fragment1 extends Fragment {
-            }
-
             @Override
             public void onClick(View view) {
-                replaceFragment(new fragment1());
+                replaceFragment(new Fragment1());
             }
         });
 
         secondButton.setOnClickListener(new View.OnClickListener() {
-            class fragment2 extends Fragment {
-            }
-
             @Override
             public void onClick(View view) {
-                replaceFragment(new fragment2());
+                replaceFragment(new Fragment2());
             }
         });
     }
@@ -44,7 +39,8 @@ public class MessageGroup extends AppCompatActivity {
     private void replaceFragment(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.framelayout, fragment);
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
         fragmentTransaction.commit();
     }
+
 }
